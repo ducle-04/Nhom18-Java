cfb8c50ee0982f18701d1c15ee8c395b992e9b97
ducle-04 <duclele004@gmail.com>
2025-05-10 22:33:11 +0700

    update

---
+++ food-ecommerce/src/pages/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="container mt-5">
+            <h1 className="text-primary">🌍 My Travel Website</h1>
+        </div>
+    );
+}
+
+export default App;
